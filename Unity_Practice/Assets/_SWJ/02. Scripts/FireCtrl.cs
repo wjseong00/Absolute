78b56a07ff189caa69e43ca201f9291684241110
wonjin0217 <yssung021@gmail.com>
2020-06-01 17:44:54 +0900

    총알 발사 로직
    - 총알 발사 구현
    - 총알 궤적 구현
    - 총알 탄피 구현
    - 총알 충돌 구현
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

---
+++ Unity_Practice/Assets/_SWJ/02. Scripts/FireCtrl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    //총알 프리팹
+    public GameObject bullet;
+    //탄피 추출 파티클
+    public ParticleSystem cartridge;
+    //총알 발사 좌표
+    public Transform firePos;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //마우스 왼쪽 버튼을 클릭했을 때 Fire 함수 호출
+        if(Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        //Bullet 프리팹을 동적으로 생성
+        Instantiate(bullet, firePos.position, firePos.rotation);
+        //파티클 실행
+        cartridge.Play();
+    }
+}
